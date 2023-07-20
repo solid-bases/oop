b42a0d7a3beb8502bd9887226b63d1a29f978c58
Fabio Cocchi <fabio.cocchi90@gmail.com>
2023-07-20 16:19:15 +0200

    chore: refactor new bricks row

--- Bricklayer/WallBuilder.cs
+++ Bricklayer/WallBuilder.cs
@@ -66,7 +66,7 @@ internal class WallBuilder
         Brick currentBrick = NewRedParallelepipedBrick();
         while (builtWidth < totalWidth)
         {
-            bool lastCol = builtWidth + NewRedParallelepipedBrick().Width >= totalWidth;
+            bool lastCol = builtWidth + currentBrick.Width >= totalWidth;
 
             currentBrick = PlaceBrickInRow(currentColNumber, currentRowNumber, lastCol);
             currentRow[currentColNumber - 1] = currentBrick;
